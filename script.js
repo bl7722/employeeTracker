3ce31c734e7654d633273fcfc9b702ba0486b1c2
Brandon Lopez <brandon.i.lopez77@gmail.com>
2020-06-10 20:52:18 -0500

    pushing new files

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-var inquirer = require('inquirer');
-var mysql = require("mysql");
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "brandonSwim1!",
+    database: "tracker_db"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    tracker();
+  });
