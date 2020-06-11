9f9cf9bb3d9f22bdee8678532d2fbca7421fe3c0
Brandon Lopez <brandon.i.lopez77@gmail.com>
2020-06-10 22:03:01 -0500

    pushing new files

--- script.js
+++ script.js
@@ -14,3 +14,28 @@ connection.connect(function(err) {
     console.log("connected as id " + connection.threadId + "\n");
     tracker();
   });
+//var to const
+  tracker()=> {
+    var emp = [];
+    var role = [];
+
+  }
+
+inquirer.prompt(
+    {
+    type:"list",
+    name:"task",
+    message:"Select Task",
+    choices: ["Add New Data", "View Current Data", "Update Employee Roles", "Exit"]
+    }
+).then(function(response){
+    
+if(response.command === "Add New Data){
+
+inquirer.prompt(
+    {
+    type:"list",
+    name:"addNewData",
+    message: "Where Adding?",
+    choices: ["Departments", "Roles", "Employees"]
+    }
