03cff0917c2c27ea605e864e813bdc34c7b014dc
Brandon Lopez <brandon.i.lopez77@gmail.com>
2020-06-12 00:31:14 -0500

    pushing new files

--- server.js
+++ server.js
@@ -2,25 +2,24 @@ const inquirer = require("inquirer");
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "brandonSwim1!",
-    database: "tracker_db"
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "brandonSwim1!",
+  database: "tracker_db"
 });
 
 connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
-    tracker();
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  tracker();
 });
 
-  function tracker(){
-        viewFullData();
-        getData();
-        const employees = [];
-        const roles = [];
-    
+function tracker(){
+    viewFullData();
+    getData();
+    const employees = [];
+    const roles = [];
 
 // First Question
     inquirer.prompt(
